be8df6b472e8ffbaab132331871ed52808c0608d
XuGuoli <guoli.xgl@alibaba-inc.com>
2017-01-10 14:04:01 +0800

    访问目录权限demo完成

--- bdchangedandperchangeddemo/src/main/java/com/scau/beyondboy/bdchangedandperchangeddemo/MainActivity.java
+++ bdchangedandperchangeddemo/src/main/java/com/scau/beyondboy/bdchangedandperchangeddemo/MainActivity.java
@@ -5,17 +5,27 @@ import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.nfc.Tag;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setNetWorkJobWork();
+        accessPrivateFile();
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -28,5 +38,16 @@ public class MainActivity extends AppCompatActivity {
                 .setRequiresCharging(true).build();
         //满足上面了两种情况就会执行这个任务
         jobScheduler.schedule(jobInfo);
+    }
+
+    //在android 7.0会引起崩溃
+    public void accessPrivateFile(){
+        //SharedPreferences preference=getSharedPreferences("test",MODE_WORLD_WRITEABLE);
+        try {
+            FileOutputStream fos = openFileOutput("test", Context.MODE_WORLD_WRITEABLE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.d(TAG,"异常问题");
+        }
     }
 }
